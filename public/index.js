2d4f47a499e614952bac8b55ad1f28b33690e5cd
Shota Ruo <scruo@ucdavis.edu>
2024-09-05 01:07:02 -0700

    commit

--- public/index.js
+++ public/index.js
@@ -8,5 +8,6 @@ function changeImage(newSrc) {
 
 
 document.getElementById("donate-btn").addEventListener("click", function(){
+    console.log("this works bruh");
     window.location.href = "https://account.venmo.com/u/Evan-Feldman-13";
 });

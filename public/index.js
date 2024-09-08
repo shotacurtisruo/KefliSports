ae5ca055d6f6e882f3620072491d6623d1c5c237
Shota Ruo <scruo@ucdavis.edu>
2024-09-08 03:35:12 -0700

    commit message update for the night

--- public/index.js
+++ public/index.js
@@ -11,3 +11,6 @@ document.getElementById("donate-btn").addEventListener("click", function(){
     console.log("this works bruh");
     window.location.href = "https://account.venmo.com/u/Evan-Feldman-13";
 });
+let donorName = document.getElementById("donor-name");
+//add a star icon next to tthe donors name for each donation make it into  a loop
+// also make sure to add as special star for first donation 

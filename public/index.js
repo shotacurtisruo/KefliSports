bc3e814dbe3de126f0c7baec9ac0d0dd8414ba00
Shota Ruo <scruo@ucdavis.edu>
2024-09-09 20:48:08 -0700

    event default and donor names update

--- public/index.js
+++ public/index.js
@@ -14,3 +14,6 @@ document.getElementById("donate-btn").addEventListener("click", function(){
 let donorName = document.getElementById("donor-name");
 //add a star icon next to tthe donors name for each donation make it into  a loop
 // also make sure to add as special star for first donation 
+const starEmoji = '⭐';
+const specialStarEmoji = '🌟'
+donorName.textContent += ' '  + specialStarEmoji;
